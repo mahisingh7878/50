357cb292f31d7ce4e0bc0ab50344267e68019c80
mahisingh4415 <mahisinghadityasingh1232@gmail.com>
2024-12-13 21:35:52 +0530

    actvity2

---
+++ actvity2.py
@@ -0,0 +1,21 @@
+file=open('mahi.txt','r')
+print(file.read())
+file.close()
+
+
+#read only the first 7 lines
+file=open('mahi.txt','r')
+print(file.readline())
+print(file.readline())
+print(file.readline())
+file.close()
+
+file=open('mahi.txt','r')
+for line in file:
+    print(line)
+
+
+file.close(``)
+
+
+
